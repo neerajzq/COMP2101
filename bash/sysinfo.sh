b5ca6d2a2c92fe53a19a9a92830d301c4742f321
neeraj <neerajkumarcad@gmail.com>
2022-06-02 22:19:07 -0400

    sysinfo

---
+++ bash/sysinfo.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# My script - sysinfo.sh
+echo '$ ./sysinfo.sh 
+Host Name: neeraj
+Domain Name: 
+Operating System name and version:
+Operating System: Ubuntu 22.04 LTS                
+Kernel: Linux 5.15.0-33-generic
+IP Addresses:
+192.168.140.129 fe80::a3ad:ccb:bd37:fbe8 
+Root Filesystem Status:
+Filesystem      Size  Used Avail Use% Mounted on
+/dev/sda3        20G  8.7G  9.4G  48% /'
