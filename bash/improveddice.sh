cc4e47556de3851c78d9d8b914195ad8e7aff249
neeraj <neerajkumarcad@gmail.com>
2022-06-09 18:56:04 -0400

    i have downloaded improvedice file and extracted it.i edited the script and checked the output

---
+++ bash/improveddice.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+#
+# this script rolls a pair of six-sided dice and displays both the rolls
+#
+
+# Task 1:
+#  put the number of sides in a variable which is used as the range for the random number
+#  put the bias, or minimum value for the generated number in another variable
+#  roll the dice using the variables for the range and bias i.e. RANDOM % range + bias
+numberSides=6
+minValue=1
+
+die=$(( RANDOM % numberSides + minValue))
+echo "Rolled $die"
+
+# Task 2:
+#  generate the sum of the dice
+#  generate the average of the dice
+
+#  display a summary of what was rolled, and what the results of your arithmetic were
+
+# Tell the user we have started processing
+echo "Rolling..."
+# roll the dice and save the results
+die1=$(( RANDOM % 6 + 1))
+die2=$(( RANDOM % 6 + 1 ))
+sum=$(( $die1 + $die2 ))
+average=$(( $sum / 2 ))
+# display the results
+echo "Rolled $die1, $die2"
+echo "Sum is $sum"
+echo "Average is $average"
+
