dd0640884e404c56316bffa3f214b7637e69d81e
neerajzq <105562704+neerajzq@users.noreply.github.com>
2022-08-17 22:10:19 -0400

    lab5

---
+++ powershell/lab5.ps1
@@ -0,0 +1,32 @@
+﻿Param 
+( 
+    [switch]$system, 
+    [switch]$disks,
+    [switch]$network
+)
+
+
+
+if ($system)
+{
+    
+    system
+} 
+elseif ($disks) 
+{
+    disk 
+}
+ elseif ($network) 
+{
+    network 
+}
+
+else
+{
+    welcome
+    get-cpuinfo
+    get-mydisks
+    system
+    network
+    disks
+}
